b1dad863095dbb0edd3cbeee5dd100b7f5345331
Vamz Gamestation <ssjshivam2777@gmail.com>
2025-04-12 18:59:07 +0530

    Add sound to js

--- Section 17 Drum Website/Drum Kit Starting Files/index.js
+++ Section 17 Drum Website/Drum Kit Starting Files/index.js
@@ -1,7 +1,10 @@
 var numberofDrums = document.querySelectorAll(".drum").length;
 
+
 for (i = 0; i<numberofDrums; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        alert(i + " drum got clicked");
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
     });
     }
+
